a7ba3ba177c0bae1df157b5ea7be628d4493d3fe
Sergei <95417457+nowelflantier@users.noreply.github.com>
2023-08-11 17:41:48 +0200

    rectif mauvais push

--- app/page.jsx
+++ app/page.jsx
@@ -12,7 +12,7 @@ const Home = () => {
       {/* <h2 className="head_text text-center">{User?.username}</h2> */}
       <p className="desc text-center">Promptopia is an open-source AI prompting tool for modern world to discover, create and share creative prompts.</p>
       <Feed />
-    </section>
+    </section>  
   );
 };
 
