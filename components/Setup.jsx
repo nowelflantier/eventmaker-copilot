2b6ed8de47d406d5848eea897c9eebdbad377c1f
Sergei <95417457+nowelflantier@users.noreply.github.com>
2023-08-14 00:25:06 +0200

    bloc setup et ajout du userContext pour gérer la présence de token

--- components/Setup.jsx
+++ components/Setup.jsx
@@ -8,7 +8,7 @@ const Setup = () => {
   return (
     <div>
       {user && !token && (
-        <div className="relative p-10 mt-10 col-span-1 overflow-hidden rounded-xl border border-gray-200 bg-white shadow-md md:col-span-2">
+        <div className="prompt_card mt-10 mb-10">
             <div className="mx-auto max-w-md text-center">
           <h3 className="text-center">
             <span className="green_gradient text-center font-semibold">Bonjour {user} !</span>
