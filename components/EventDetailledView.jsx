1d3e8a1680b1e2d7215d6f2799a8881e4da2a602
Sergei <95417457+nowelflantier@users.noreply.github.com>
2023-08-16 12:43:32 +0200

    réorg affichage vue détaillée + gestion de la mise à jour des données depuis EM au chargement de la vue

--- components/EventDetailledView.jsx
+++ components/EventDetailledView.jsx
@@ -34,11 +34,6 @@ const EventDetailledView = ({ event, isEventLoaded }) => {
 
   return (
     <div className="relative isolate w-11/12  glassmorphism mb-10 overflow-hidden bg-gray-900 py-24 sm:py-32">
-      {/* <img
-        src="https://images.unsplash.com/photo-1521737604893-d14cc237f11d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&crop=focalpoint&fp-y=.8&w=2830&h=1500&q=80&blend=111827&sat=-100&exp=15&blend-mode=multiply"
-        alt=""
-        className=" absolute opacity-50 blur-lg inset-0 -z-10 h-full w-full object-cover object-right md:object-center"
-      /> */}
       <div
         className="hidden sm:absolute sm:-top-10 sm:right-1/2 sm:-z-10 sm:mr-10 sm:block sm:transform-gpu sm:blur-3xl"
         aria-hidden="true"
@@ -74,27 +69,7 @@ const EventDetailledView = ({ event, isEventLoaded }) => {
             {event?.description}
           </p>
         </div>
-        {!event?.type_of_content ? (
-          <Link
-            className=" prompt_cta_card text-center"
-            href={`/event/${event?._id}/edit`}
-          >
-            <span className=" cta_text p-1 ">
-              Générer les contenus de mon évènement avec Eventmaker Copilot{" "}
-              <span aria-hidden="true">&rarr;</span>
-            </span>
-          </Link>
-        ) : (
-          <Link
-            className=" prompt_cta_card text-center"
-            href={`/event/${event?._id}/edit`}
-          >
-            <span className=" cta_text p-1 ">
-              Modifier les informations de votre évènement{" "}
-              <span aria-hidden="true">&rarr;</span>
-            </span>
-          </Link>
-        )}
+       
         <div className="mx-auto mt-10 max-w-2xl lg:mx-0 lg:max-w-none">
           <div className="grid grid-cols-1 gap-x-8 gap-y-6 text-base font-semibold leading-7 text-gray-800 sm:grid-cols-2 md:flex lg:gap-x-10">
             {links.map((link) => (
@@ -118,7 +93,35 @@ const EventDetailledView = ({ event, isEventLoaded }) => {
               </div>
             ))}
           </dl>
+          
           <dl className="mt-6 grid grid-cols-1 card_container gap-8 sm:mt-10 sm:grid-cols-2 lg:grid-cols-4">
+          <h2 className="text-2xl font-bold text-center tracking-tight text-white sm:text-6xl">
+            <span className="green_gradient">Votre contenu personnalisé </span>
+            {/* <br className="leading-3"/> */}
+           
+          </h2>
+          <p className="ext-2xl leading-3 font-bold text-center tracking-tight text-white sm:text-6xl"> <span className="black_gradient text-2xl">pour votre évènement</span></p>
+          {!event?.type_of_content ? (
+          <Link
+            className=" prompt_cta_card text-center"
+            href={`/event/${event?._id}/edit`}
+          >
+            <span className=" cta_text p-1 ">
+              Générer les contenus de mon évènement avec Eventmaker Copilot{" "}
+              <span aria-hidden="true">&rarr;</span>
+            </span>
+          </Link>
+        ) : (
+          <Link
+            className=" prompt_cta_card text-center"
+            href={`/event/${event?._id}/edit`}
+          >
+            <span className=" cta_text p-1 ">
+              Modifier les informations de votre évènement{" "}
+              <span aria-hidden="true">&rarr;</span>
+            </span>
+          </Link>
+        )}
             {event?.type_of_event &&
               data.map((stat) => (
                 <div key={stat.name} className="flex card flex-col-reverse">
--- components/FetchEvents.jsx
+++ components/FetchEvents.jsx
@@ -1,16 +1,20 @@
-'use server'
+"use server";
 import Event from "@models/events";
 import { connectToBD } from "@utils/database";
 
 export async function fetchEventsFromServer(token) {
-    const res = await fetch(`https://app.eventmaker.io/api/v1/events.json?per_page=20&auth_token=${token}`);
-    const events = await res.json();
-    return events
-  }
+  const res = await fetch(
+    `https://app.eventmaker.io/api/v1/events.json?per_page=20&auth_token=${token}`
+  );
+  const events = await res.json();
+  return events;
+}
 
-export async function fetchEventsDetailsFromServer({token, eventId}) {
-    // Récupérer les détails de l'événement depuis Eventmaker
-  const res = await fetch(`https://app.eventmaker.io/api/v1/events/${eventId}.json?auth_token=${token}`);
+export async function fetchEventsDetailsFromServer({ token, eventId }) {
+  // Récupérer les détails de l'événement depuis Eventmaker
+  const res = await fetch(
+    `https://app.eventmaker.io/api/v1/events/${eventId}.json?auth_token=${token}`
+  );
   const eventsDetailsFromEventmaker = await res.json();
 
   // Connecter à la base de données
@@ -21,10 +25,14 @@ export async function fetchEventsDetailsFromServer({token, eventId}) {
 
   if (existingEvent) {
     // Si le titre est différent, mettre à jour le titre dans votre base de données
-    if (existingEvent.title !== eventsDetailsFromEventmaker.title) {
-      existingEvent.title = eventsDetailsFromEventmaker.title;
-      await existingEvent.save();
-    }
+
+    existingEvent.title = eventsDetailsFromEventmaker.title;
+    existingEvent.organizer = eventsDetailsFromEventmaker.organizer;
+    existingEvent.start_date = eventsDetailsFromEventmaker.start_date;
+    existingEvent.end_date = eventsDetailsFromEventmaker.end_date;
+    
+    
+    await Event.findByIdAndUpdate(eventId, existingEvent);
   } else {
     // Si l'événement n'existe pas dans votre base de données, vous pouvez le créer ici si nécessaire
     // ...
@@ -32,5 +40,4 @@ export async function fetchEventsDetailsFromServer({token, eventId}) {
 
   // Retourner l'objet avec les informations à jour
   return existingEvent ? existingEvent : eventsDetailsFromEventmaker;
-    
 }
