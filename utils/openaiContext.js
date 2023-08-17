d69717e8220341e5c5624a4031a6dfdabef70822
Sergei <95417457+nowelflantier@users.noreply.github.com>
2023-08-17 18:40:28 +0200

    ajout de la v1 de génération de contenu, bridé en terme de tokens, ajustement à prévoir pour le chargement

--- utils/openaiContext.js
+++ utils/openaiContext.js
@@ -24,7 +24,7 @@ export const callOpenAI = async (text) => {
               }
         ],
         temperature: 1,
-        max_tokens: 1000,
+        max_tokens: 600,
         top_p: 1,
         frequency_penalty: 0,
         presence_penalty: 0,
